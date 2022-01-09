c299195f51057c82857ff17828d772b01d0989d3
dmknapp2385 <dmknapp2385@gmail.com>
2022-01-09 15:16:09 -0700

    employee test

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -1,13 +1,10 @@
 const { TestWatcher } = require('jest');
 const Employee = require('../lib/Employee.js');
 
-jest.mock('../lib/Employee.js');
-
-console.log(new Employee);
 
 test('employee object created', () => {
-    const employee = new Employee;
-    expect(employee.name).toBe('John');
-    expect(employee.id).toBe(2354);
-    expect(employee.email).toBe("jdoe@gmail.com");
+    const employee = new Employee();
+    expect(employee.name).toEqual(expect.any(String));
+    expect(employee.id).toEqual(expect.any(String));
+    expect(employee.email).toEqual(expect.any(String));
 });
