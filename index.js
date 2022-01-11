17973316fcba36878659b587f57f50b0443979e0
dmknapp2385 <dmknapp2385@gmail.com>
2022-01-10 17:49:53 -0700

    other personel classes added

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
 const Employee = require('./lib/Employee');
 const person = new Employee;
- person.getName()
-    .then(person.getId)
 
-
-
-
-// const inquirer = require('inquirer');
+person.getName()
+   .then(person.getId);
 
 
 
@@ -15,32 +11,3 @@ const person = new Employee;
 
 
 
-// const getInfo = function() {
-//     return inquirer.
-//     prompt([
-//         {
-//             input: 'text', 
-//             name:'name', 
-//             message: 'What is the employee name',
-//         },
-//         {
-//             input: 'text', 
-//             name:'id', 
-//             message: 'What is the employee id',
-//         },
-//         {
-//             input: 'text', 
-//             name:'email', 
-//             message: 'What is the employee email',
-//         }
-// ]);
-
-// }
-
-// getInfo()
-//     .then(data => {
-//         employee.name = data.name;
-//         employee.id =data.id;
-//         employee.email = data.email
-//         console.log(employee.name)
-//     })
